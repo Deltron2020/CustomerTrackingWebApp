5e60d1fdc5996914ae545b4f32b869e90646028e
ttrice <ttrice.analyst@gmail.com>
2024-09-13 16:33:09 -0400

    worked on sample ticket search using ajax data exchange rather than post form submission allowing for parameters to stay and data to load into table wo page refresh

--- app/ct_update_ticket/routes.py
+++ app/ct_update_ticket/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, request, session, redirect, url_for, jsonify, flash
+import json
 from app.ct_update_ticket.sql import (load_tickets_from_db, update_ticket_in_db, version_id_check, got_it_db_insert, got_it_ticket_tracking, forward_to_options, insert_correspondence_notes, load_correspondence_notes, populate_type_of_contact_selection, populate_return_call_operator_selection, populate_ticket_status_selection, populate_all_ticket_types, populate_got_it_operator_selection, check_last_got_it, populate_all_ticket_years, is_personal_property)
 from app import app
 from datetime import date, timedelta
@@ -151,6 +152,96 @@ def correspondence_notes_update(ticketNumber):
             insert_correspondence_notes(ticketNumber, data['CorrespondenceNotes'], session['username'])
             return (
                 redirect(url_for('view_searched_ticket', ticketNumber=ticketNumber)))
+
+        else:
+            return redirect(url_for('customer_tracking_login'))
+    except Exception as e:
+        return render_template('CT_Error.html', error=e)
+
+
+
+### ticket search TEST ###
+@app.route("/searchtest", methods=['GET'])
+def customer_tracking_search_test():
+    try:
+        if 'username' in session:
+            contact_types = populate_type_of_contact_selection()
+            return_operators = populate_return_call_operator_selection()
+            gotit_operators = populate_got_it_operator_selection()
+            status_options = populate_ticket_status_selection()
+            ticketTypes = populate_all_ticket_types()
+            ticketYears = populate_all_ticket_years()
+            data = request.args
+            ticket = {}
+            for i in data:
+                if (len(data[i])) > 0:
+                    search = {i: data[i]}
+                    ticket.update(search)
+
+            columns = ' AND '.join(f"{column} LIKE :?" for column in ticket.keys())
+            for i in ticket:
+                columns = columns.replace('?', str(ticket[i]).replace('-','').replace(' ',''), 1)
+
+            parameters = {}
+            for val in ticket.values():
+                parameters.update({val.replace('-','').replace(' ',''): '%'+val+'%'})
+
+            if not ticket:
+                results = []
+                return render_template('CT_SearchTest.html',
+                                       ticket_data=results,
+                                       current_user=session['username'],
+                                       contact_types=contact_types,
+                                       return_operators=return_operators,
+                                       gotit_operators=gotit_operators,
+                                       status_options=status_options,
+                                       ticketTypes=ticketTypes,
+                                       ticketYears=ticketYears)
+            else:
+                results = load_tickets_from_db(columns, parameters)
+                return render_template('CT_SearchTest.html',
+                                       ticket_data=results,
+                                       current_user=session['username'],
+                                       current_date=date.today() - timedelta(days=3),
+                                       contact_types=contact_types,
+                                       return_operators=return_operators,
+                                       gotit_operators=gotit_operators,
+                                       status_options=status_options,
+                                       ticketTypes=ticketTypes,
+                                       ticketYears=ticketYears)
+
+        else:
+            return redirect(url_for('customer_tracking_login'))
+    except Exception as e:
+        return render_template('CT_Error.html', error=e)
+
+
+
+### ticket search TEST ###
+@app.route("/searchtestjson", methods=['GET'])
+def customer_tracking_search_test_json():
+    try:
+        if 'username' in session:
+            data = request.args
+            ticket = {}
+            for i in data:
+                if (len(data[i])) > 0:
+                    search = {i: data[i]}
+                    ticket.update(search)
+
+            columns = ' AND '.join(f"{column} LIKE :?" for column in ticket.keys())
+            for i in ticket:
+                columns = columns.replace('?', str(ticket[i]).replace('-','').replace(' ',''), 1)
+
+            parameters = {}
+            for val in ticket.values():
+                parameters.update({val.replace('-','').replace(' ',''): '%'+val+'%'})
+
+            results = load_tickets_from_db(columns, parameters)
+
+            json_data = json.dumps(results, indent=4, sort_keys=True, default=str)
+
+            return json_data
 
         else:
             return redirect(url_for('customer_tracking_login'))
